5e8152af618c4565a3b53c8007fbc0885750f0d9
michael j. Walker & Kailash & laomatt & Missling <pair+Neutrix.kduraiswami.laomatt.missling@devbootcamp.com>
2015-03-19 16:44:54 -0700

    Confirm seeding of database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-proto=User.create(username:"prototypo", pw:"allyourbases")
+proto=User.create(username:"prototypo", password:"allyourbases")
 
 cards1=[
 {question:"What are the house of Greyjoy words?",answer:"We do not sow", dummy_answer_1:"Theons a douche", dummy_answer_2:"Dead men tell no tales", dummy_answer_3:"What is dead may never die"},
