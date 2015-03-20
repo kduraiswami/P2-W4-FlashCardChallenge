ec7f4125000153a567f7daa88f7097fb3414afac
Kailash <kaidurai@gmail.com>
2015-03-20 08:42:15 -0700

    updated validation tests for user and card models, updated index route tests for /play route - still not working

--- spec/user_test.rb
+++ spec/user_test.rb
@@ -1,4 +1,4 @@
-require 'spec helper'
+require_relative 'spec_helper'
 
 describe "User Model" do
 
@@ -11,5 +11,29 @@ describe "User Model" do
     end
 
   end
+
+  context 'user enters invalid account information' do
+
+    it "validation should not allow a password less than 6 characters" do
+      expect{
+        User.create(username: "sample1", password: "(0...3).map { (65 + rand(26)).chr }.join")
+      }.to change { User.count }.by(0)
+    end
+
+    it "validation should not allow a password without a number" do
+      expect{
+        User.create(username: "sample1", password: "(0...8).map { (65 + rand(26)).chr }.join")
+      }.to change { User.count }.by(0)
+    end
+
+    it "validation should not allow a username without a number" do
+      expect{
+        User.create(username:"(0...8).map { (65 + rand(26)).chr }.join", password: "(0...8).map { (65 + rand(26)).chr }.join + 1")
+      }.to change { User.count }.by(0)
+    end
+
+  end
+
+end
 
 
