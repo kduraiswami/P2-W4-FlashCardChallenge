19f567d89204ec9ef794b3cb6892df32632cb7a8
laomatt <laomatt1@gmail.com>
2015-03-19 20:21:05 -0700

    add witty messages into a @feedback variable for user experience

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -16,11 +16,20 @@ post '/submit' do
   p @id=params[:current_id].to_i
   p @answer=Deck.where(name:"Game of thrones").first.cards[@id].answer
   p params[:answer]
+  # if @answer == params[:answer]
+  #   @id+=1
+  # #   session['current_score']+=1
+  # @id=params[:current_id]
   if @answer == params[:answer]
     @id+=1
-  #   session['current_score']+=1
-  @id=params[:current_id]
+    session['current_score']+=1
+    @feedback = "You choose correctly."
+  else
+    @id+=1
+    @feedback = "You fucked up that last question, the answer was: #{@answer}"
+  end
 
+  redirect '/'
   redirect "/play/#{@id}"
   # redirect '/play/@id'
 end
