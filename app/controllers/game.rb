9be19ac9be2a7e9828302eb96f8cd093a3bde20c
michael j. Walker & Kailash & laomatt & Missling <pair+Neutrix.kduraiswami.laomatt.missling@devbootcamp.com>
2015-03-19 19:14:26 -0700

    Update view and working submit button

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -3,23 +3,22 @@ erb :index
 end
 
 get '/play/:id' do
-  # session['id']`
+  # session['id']
   # session['current_score']=0
     @question=Deck.where(name:"Game of thrones").first.cards[params[:id].to_i]
     @deck=Deck.where(name:"Game of thrones").first
   erb :game
 end
 
-
 post '/submit' do
-  @id=params[:current_id]
-  if @answer == params[:answer]
-    @id+=1
-    session['current_score']+=1
-  else
-    @id+=1
-  end
+  # @id=params[:current_id]
+  # if @answer == params[:answer]
+  #   @id+=1
+  #   session['current_score']+=1
+  # else
+  #   @id+=1
+  # end
 
-  # redirect '/'
-  redirect '/play/@id'
+  redirect '/'
+  # redirect '/play/@id'
 end
